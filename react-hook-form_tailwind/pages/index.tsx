d80c38bd0b07ae1ad1dc1e7aa422cb02c1f67c51
prac2317 <lowdy92@naver.com>
2024-04-13 11:06:21 +0900

    기능 구현 완료

--- react-hook-form_tailwind/pages/index.tsx
+++ react-hook-form_tailwind/pages/index.tsx
@@ -1,118 +1,161 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+// import { Inter } from 'next/font/google';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [submitted, setSubmitted] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  const onValid = (data) => {
+    setSubmitted(true);
+    setFormData(data);
+  };
+
+  const onInvalid = (errors) => {
+    setSubmitted(false);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center items-center">
+      <form
+        id="form"
+        className=" border-4 border-black border-r-8 rounded-2xl w-[500px] h-[1000px] p-8 flex flex-col  bg-pink-200 font-extrabold"
+        onSubmit={handleSubmit(onValid, onInvalid)}
+      >
+        <div className="flex justify-center items-center mb-8">
+          <div className="text-3xl">Job Application Form</div>
         </div>
-      </div>
+        <div className="mb-8">
+          <div className="mb-2">What department do you want to work for?</div>
+          <div>
+            <input
+              {...register('department')}
+              type="radio"
+              name="department"
+              value="sales"
+            />
+            <span>Sales</span>
+          </div>
+          <div>
+            <input
+              {...register('department')}
+              type="radio"
+              name="department"
+              value="marketing"
+            />
+            Marketing
+          </div>
+          <div>
+            <input
+              {...register('department')}
+              type="radio"
+              name="department"
+              value="according"
+            />
+            According
+          </div>
+          <div>
+            <input
+              {...register('department')}
+              type="radio"
+              name="department"
+              value="customer service"
+            />
+            Customer Service
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="mb-8">
+          <div className="mb-2">Why do you want to join this company?</div>
+          <div>
+            <input {...register('why')} type="radio" name="why" value="money" />
+            <span>I want money!</span>
+          </div>
+          <div>
+            <input {...register('why')} type="radio" name="why" value="love" />I
+            love this company
+          </div>
+          <div>
+            <input {...register('why')} type="radio" name="why" value="learn" />
+            I want to learn
+          </div>
+          <div>
+            <input {...register('why')} type="radio" name="why" value="why" />I
+            don't know why
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="w-full mb-4">
+          <div className="mb-2">Salary</div>
+          <select
+            {...register('salary')}
+            className="border-4 border-black rounded-md w-full"
+          >
+            <option value="$50K">$50K</option>
+            <option value="$100K">$100K</option>
+            <option value="$150K">$150K</option>
+            <option value="$200K">$200K</option>
+          </select>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="mb-4">
+          <div>Intoduce yourself</div>
+          <input
+            {...register('introduce', {
+              required: 'Please write down your introduction',
+            })}
+            type="text"
+            className="border-4 border-black rounded-md w-full"
+          />
+          <div className="text-red-700">{errors.introduce?.message}</div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        <div className="mb-4">
+          <div>Tell us what your dreams are</div>
+          <textarea
+            {...register('dreams', {
+              required: 'Please write down your dreams',
+              minLength: {
+                message: 'Please write more than 10 characters.',
+                value: 10,
+              },
+            })}
+            className="border-4 border-black rounded-md w-full"
+          />
+          <div className="text-red-700">{errors.dreams?.message}</div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="mb-4">
+          <div>Email</div>
+          <input
+            {...register('email', {
+              required: 'Please write down your email',
+              validate: {
+                onlyNaver: (value) =>
+                  value.includes('@naver') || 'Only @naver is allowed.',
+              },
+            })}
+            type="text"
+            className="border-4 border-black rounded-md w-full"
+          />
+          <div className="text-red-700">{errors.email?.message}</div>
+        </div>
+
+        <button className="border-2 border-r-4 border-b-4 border-black h-16 bg-yellow-400 mb-5">
+          Give me this job
+        </button>
+        <div className="w-full break-all">
+          {submitted ? ` ${JSON.stringify(formData)}` : ''}
+        </div>
+      </form>
+    </div>
   );
 }
